120922867db78df92fb9cabed6464a578a49ca50
Pradhan <chrisonpradhan@gmail.com>
2022-03-29 17:46:34 +0530

    Add more Crowdsale tests

--- test/PillowCrowdsale.test.js
+++ test/PillowCrowdsale.test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { BigNumber } = require("ethers");
 const { ethers } = require("hardhat");
 
 
@@ -48,6 +49,41 @@ describe("PillowTokenCrowdsale", function () {
 
         expect(await crowdsale.stage()).to.equal(0);
       });
+  
+      it("wallet should equal owner ", async function () {
+        const [owner] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("PillowToken");
+        const token = await Token.deploy();
+        await token.deployed();
+        const Crowdsale = await ethers.getContractFactory("PillowCrowdsale");
+        const crowdsale = await Crowdsale.deploy("40000",owner.address,token.address);
+        await crowdsale.deployed();
+
+        expect(await crowdsale.wallet()).to.equal(owner.address);
+      });
+
+      it("should transfer tokens to crowsale address ", async function () {
+        const [owner,addr1] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("PillowToken");
+        const token = await Token.deploy();
+        await token.deployed();
+        const Crowdsale = await ethers.getContractFactory("PillowCrowdsale");
+        const crowdsale = await Crowdsale.deploy("40000",owner.address,token.address);
+        await crowdsale.deployed();
+       
+        await token.connect(owner).transfer(crowdsale.address, "100000000000000000000000000");
+        expect(await token.balanceOf(crowdsale.address)).to.equal("100000000000000000000000000");
+        const transactionHash = await addr1.sendTransaction({
+          to: crowdsale.address,
+          value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+        });
+        expect(await token.balanceOf(addr1.address)).to.equal("40000000000000000000000");
+
+      });
+
+
+
+
   
   
 
