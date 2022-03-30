30e4c30dbb7293b3f0a8b1a92be51306a013b5dc
Pradhan <chrisonpradhan@gmail.com>
2022-03-30 09:39:37 +0530

    contract modifications

--- contracts/PillowToken.sol
+++ contracts/PillowToken.sol
@@ -1,4 +1,3 @@
-
 // contracts/ExampleToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
@@ -9,8 +8,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 //The name of the token is  PillowToken and the symbol is PILW
 
 contract PillowToken is ERC20 {
-constructor () ERC20("PillowToken", "PILW")
-{
-_mint(msg.sender,100000000 * 10 ** decimals());
-}
+    constructor() ERC20("PillowToken", "PILW") {
+        _mint(msg.sender, 100000000 * 10**decimals());
+    }
 }
